93f576d58b666b55437537ca2df938eba86ab779
Paul Rock <paul@drteam.rocks>
2018-04-11 15:32:16 +0300

    index page added in example

--- public/index.php
+++ public/index.php
@@ -9,6 +9,7 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 $app = new \DrMVC\App($config);
 $app
+    ->get('/', \MyApp\Controllers\Index::class)
     ->get('/zzz', \MyApp\Controllers\Index::class)
     ->get('/zzz/<action>', \MyApp\Controllers\Index::class)
     ->get('/aaa', function(Request $request, Response $response, $args) {
